4aeefa4097fe58efb87d98f53563c9080dd22bdf
Denis <denisgreshnaykov@gmail.com>
2022-12-02 00:01:38 +0500

    add text-editor

--- dist/script.js
+++ dist/script.js
@@ -1827,7 +1827,24 @@ class Editor extends react__WEBPACK_IMPORTED_MODULE_2__.Component {
   open(page) {
     this.currentPage = `../${page}`;
     this.iframe.load(this.currentPage, () => {
-      console.log(this.currentPage);
+      const body = this.iframe.contentDocument.body;
+      let textNodes = [];
+      function recursy(element) {
+        element.childNodes.forEach(node => {
+          if (node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
+            textNodes.push(node);
+          } else {
+            recursy(node);
+          }
+        });
+      }
+      recursy(body);
+      textNodes.forEach(node => {
+        const wrapper = this.iframe.contentDocument.createElement("text-editor");
+        node.parentNode.replaceChild(wrapper, node);
+        wrapper.appendChild(node);
+        wrapper.contentEditable = "true";
+      });
     });
   }
   loadPageList() {
